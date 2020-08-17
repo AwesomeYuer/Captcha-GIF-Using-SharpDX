1685bd9a64b4e9599c3770e703f23ca3aaeb6db6
Microshaoft <microshaoft@msn.com>
2020-08-17 16:47:51 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TestConsoleApplication
+{
+    using SharpDX;
+    using SharpDX.Win32;
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using D2D = SharpDX.Direct2D1;
+    using DWrite = SharpDX.DirectWrite;
+    using WIC = SharpDX.WIC;
+    using Microshaoft;
+    class Program
+    {
+        static void Main()
+        {
+            byte[] gif = CaptchaGifSharpDxHelper.SaveD2DBitmap(200, 100, "难得糊涂222");
+            File.WriteAllBytes(@"\test.gif", gif);
+            //Util.Image(gif).Dump();
+        }
+
+        
+
+    }
+
+
+    
+    
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+ 
+ # Non-Windows platform Unsupported
+ # References and Thanks:
+
+ # 《.NET中生成动态验证码》
+ https://www.cnblogs.com/sdflysha/p/dotnet-better-captcha.html
+
+ 
+ # 《和SharpDX坑爹的Variant刚正面》
+ https://www.cnblogs.com/sdflysha/p/20191127-the-variant-bug-in-sharpdx.html
+ https://raw.githubusercontent.com/sdcb/blog-data/master/2019/20191127-the-variant-bug-in-sharpdx/WIC-Captcha-Gif-Ripple-Loop.linq
+ 
+ # 《The .NET Programmer’s Playground》
+ https://www.linqpad.net/
+ 
